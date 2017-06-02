51668b176685d9689171afb24b3357bab5047213
ases573 <ases573@gmail.com>
2017-06-02 13:59:59 +0530

    first Commit

---
+++ js/scripts.js
@@ -0,0 +1,91 @@
+function startGame()
+{
+    for (var i = 1; i <= 9; i++)
+    {
+        clearBox(i);
+    }
+    document.turn = "X";
+    if (Math.random() < 0.5)
+    {
+        document.turn = "O";
+    }
+    document.winner = null;
+    setMessage(document.turn + "'s Turn");
+    bootbox.alert(document.turn + "'s Turn");
+}
+
+function setMessage(msg)
+{
+    $("#message").html(msg);
+}
+
+function nextMove(square)
+{
+    if (document.winner != null)
+    {
+        setMessage(document.winner + " has already won ");
+        bootbox.alert(document.winner + " has already won ");
+    }
+    else
+    if (square.innerText == "")
+    {
+        square.innerText = document.turn;
+        switchTurn();
+    }
+    else
+    {
+        setMessage("Box already in use");
+        bootbox.alert("Box already in use");
+    }
+}
+
+function switchTurn()
+{
+    if (checkForWinner(document.turn))
+    {
+        setMessage("Congrats, " + document.turn + " has won");
+        bootbox.alert("Congrats, " + document.turn + " has won");
+        document.winner = document.turn;
+    }
+    else if (document.turn == "X")
+    {
+        document.turn = "O";
+        setMessage(document.turn + "'s Turn");
+    }
+    else
+    {
+        document.turn = "X";
+        setMessage(document.turn + "'s Turn");
+    }
+
+}
+
+function checkForWinner(move)
+{
+    var result = false;
+    if (checkRow(1, 2, 3, move) || checkRow(4, 5, 6, move) || checkRow(7, 8, 9, move) || checkRow(1, 4, 7, move) || checkRow(2, 5, 8, move) || checkRow(3, 6, 9, move) || checkRow(1, 5, 9, move) || checkRow(3, 5, 7, move))
+    {
+        result = true;
+    }
+    return result;
+}
+
+function checkRow(a, b, c, move)
+{
+    var result = false;
+    if (getBox(a) == move && getBox(b) == move && getBox(c) == move)
+    {
+        result = true;
+    }
+    return result;
+}
+
+function getBox(number)
+{
+    return document.getElementById("s" + number).innerText;
+}
+
+function clearBox(number)
+{
+    document.getElementById("s" + number).innerText = "";
+}
